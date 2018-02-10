4ad1a0bd8e3de3b1be41c408a68073e6426841a8
sergiosanpol <sergiosanpol@gmail.com>
2018-02-10 17:11:41 +0100

    Implementación de Parcelable a Monumentos

--- app/src/main/java/hlc/com/monumentosdeespaa/Datos/Monumentos.java
+++ app/src/main/java/hlc/com/monumentosdeespaa/Datos/Monumentos.java
@@ -1,14 +1,18 @@
 package hlc.com.monumentosdeespaa.Datos;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.Date;
 
 /**
+ * Clase que representa a los monumetos de la vista de la base de datos
  * Created by Sergio on 10/02/2018.
  */
 
-public class Monumentos {
+public class Monumentos implements Parcelable{
 
     private int id_monumentos;
     private String nombre;
@@ -26,6 +30,15 @@ public class Monumentos {
         this.provincia = provincia;
         this.comunidad = comunidad;
         this.latLng = new LatLng(lat, lng);
+    }
+
+    protected Monumentos(Parcel in) {
+        id_monumentos = in.readInt();
+        nombre = in.readString();
+        localidad = in.readString();
+        provincia = in.readString();
+        comunidad = in.readString();
+        latLng = in.readParcelable(LatLng.class.getClassLoader());
     }
 
     public int getId_monumentos() {
@@ -83,4 +96,31 @@ public class Monumentos {
     public void setLatLng(LatLng latLng) {
         this.latLng = latLng;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id_monumentos);
+        dest.writeString(nombre);
+        dest.writeString(localidad);
+        dest.writeString(provincia);
+        dest.writeString(comunidad);
+        dest.writeParcelable(latLng, flags);
+    }
+
+    public static final Parcelable.Creator<Monumentos> CREATOR
+            = new Parcelable.Creator<Monumentos>() {
+        public Monumentos createFromParcel(Parcel in) {
+            return new Monumentos(in);
+        }
+
+        public Monumentos[] newArray(int size) {
+            return new Monumentos[size];
+        }
+    };
+
 }
